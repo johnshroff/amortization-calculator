fd105bed26b12ad55f6dd1efa8b03ddafe728fc1
johnshroff <johnshroff@hotmail.com>
2018-12-17 11:11:07 -0600

    Add All functionality

---
+++ js/amortization.js
@@ -0,0 +1,79 @@
+function amortization(currentBalance, interestRate, termLength) {
+    this.currentBalance = currentBalance,
+    this.interestRate = interestRate,
+    this.termLength = termLength;
+    var monthlyPayment,
+    totalSavings,
+    paymentSchedule = [];
+
+    this.calculateMonthlyPayment = function() {
+      var paymentPeriods = this.termLength * 12,
+        interestPerPeriod = this.interestRate / 12;
+      monthlyPayment = currentBalance * ((interestPerPeriod * Math.pow((1 + interestPerPeriod), paymentPeriods)) / (Math.pow((1 + interestPerPeriod), paymentPeriods) -1));
+      return monthlyPayment;
+    };
+    this.calculatePaymentSchedule = function() {
+      var paymentPeriods = this.termLength * 12,
+        interestPerPeriod = this.interestRate / 12,
+        previousDefaultBalance = this.currentBalance,
+        previousActualBalance = this.currentBalance,
+        defaultInterestPaid = 0,
+        actualInterestPaid = 0;
+
+      paymentSchedule = [];
+      for (var i = 1; i < paymentPeriods; i++) {
+          var payment = {};
+
+          payment["defaultInterest"] = previousDefaultBalance * interestPerPeriod;
+          payment["defaultPrinciple"] = monthlyPayment - payment.defaultInterest;
+
+          previousDefaultBalance -= payment.defaultPrinciple;
+          defaultInterestPaid += payment.defaultInterest;
+
+          payment["defaultBalance"] = previousDefaultBalance;
+          payment["defaultInterestPaid"] = defaultInterestPaid;
+
+          if (previousActualBalance > 0) {
+            payment["actualInterest"] = previousActualBalance * interestPerPeriod;
+
+            if (parseFloat(previousActualBalance) < parseFloat(this.actualMonthlyPayment)) {
+                payment["actualPrinciple"] = previousActualBalance;
+                previousActualBalance = 0;
+            } else {
+              payment["actualPrinciple"] = this.actualMonthlyPayment - payment.actualInterest;
+              previousActualBalance -= payment["actualPrinciple"];
+            }
+
+            actualInterestPaid += payment.actualInterest;
+
+            payment["actualBalance"] = previousActualBalance;
+            payment["actualInterestPaid"] = actualInterestPaid;
+          } else {
+            payment["actualInterest"] = 0;
+            payment["actualPrinciple"] = 0;
+            payment["actualBalance"] = 0;
+            payment["actualInterestPaid"] = actualInterestPaid;
+          }
+
+          paymentSchedule.push(payment);
+      }
+
+      totalSavings = defaultInterestPaid - actualInterestPaid;
+    };
+    this.getMonthlyPayment = function() {
+      return Number(monthlyPayment).toFixed(2);
+    };
+    this.getPaymentSchedule = function() {
+      if (paymentSchedule.length == 0) {
+        this.calculatePaymentSchedule();
+      }
+      return paymentSchedule;
+    };
+    this.getTotalSavings = function () {
+      return Number(totalSavings).toFixed(2);
+    };
+
+    this.calculateMonthlyPayment();
+    this.actualMonthlyPayment = monthlyPayment;
+    totalSavings = 0;
+}
---
+++ js/dom.js
@@ -0,0 +1,74 @@
+var amo;
+$("#loanSubmit").click(function() {
+  $("#paymentScheduleTable").remove();
+  $("#overviewPanel").remove();
+  var loanAmount = $("#loanAmount").val(),
+    interestRate = $("#interestRate").val() / 100,
+    termLength = $("#termLength").val();
+
+    if (loanAmount == 0 || interestRate == 0 || termLength == 0) {
+      alert('All fields are required to create the payment schedule');
+      return;
+    }
+
+    amo = new amortization(loanAmount, interestRate, termLength);
+    amo.calculateMonthlyPayment();
+    $("#defaultPaymentLabel").val(amo.getMonthlyPayment());
+    $("#actualPaymentModal").modal();
+});
+$("#actualPaymentModal").on("shown.bs.modal", function() {
+  $("#actualPaymentLabel").focus();
+});
+$("#createPaymentScheduleButton").click(function() {
+  var table = $("<table>", {class: "table", id: "paymentScheduleTable"});
+    amo.actualMonthlyPayment = $("#actualPaymentLabel").val();
+
+  var payments = amo.getPaymentSchedule();
+
+  var overviewPanel = $("<div>", {id: "overviewPanel"}),
+    defaultPayment = $("<h4>", {text: "Default Monthly Payment: " + amo.getMonthlyPayment()}),
+    actualPayment = $("<h4>", {text: "Actual Monthly Payment: " + amo.actualMonthlyPayment}),
+    totalInterestSaved = $("<h4>", {text: "Total Interest Saved: " + amo.getTotalSavings()});
+
+  overviewPanel.append(defaultPayment);
+  overviewPanel.append(actualPayment);
+  overviewPanel.append(totalInterestSaved);
+
+  $("#main").append(overviewPanel);
+
+  var thead = $("<thead>")
+      tbody = $("<tbody>"),
+      tr = $("<tr>");
+
+  tr.append($("<th>", {text: "Payment #"}));
+  tr.append($("<th>", {text: "Default Interest"}));
+  tr.append($("<th>", {text: "Default Principle"}));
+  tr.append($("<th>", {text: "Default Balance"}));
+  tr.append($("<th>", {text: "Actual Interest"}));
+  tr.append($("<th>", {text: "Actual Principle"}));
+  tr.append($("<th>", {text: "Actual Balance"}));
+  tr.append($("<th>", {text: "Money Saved"}));
+  thead.append(tr);
+  table.append(thead);
+
+  payments.forEach(function(payment, index) {
+    var moneySaved = payment.defaultInterestPaid - payment.actualInterestPaid;
+    if (moneySaved < 0) {
+      moneySaved = '';
+    }
+
+    tr = $("<tr>");
+    tr.append($("<td>", {text: index + 1}));
+    tr.append($("<td>", {text: Number(payment.defaultInterest).toFixed(2)}));
+    tr.append($("<td>", {text: Number(payment.defaultPrinciple).toFixed(2)}));
+    tr.append($("<td>", {text: Number(payment.defaultBalance).toFixed(2)}));
+    tr.append($("<td>", {text: Number(payment.actualInterest).toFixed(2)}));
+    tr.append($("<td>", {text: Number(payment.actualPrinciple).toFixed(2)}));
+    tr.append($("<td>", {text: Number(payment.actualBalance).toFixed(2)}));
+    tr.append($("<td>", {text: Number(moneySaved).toFixed(2)}));
+    tbody.append(tr);
+  });
+  table.append(tbody);
+  $("#main").append(table);
+  $("#actualPaymentModal").modal("hide");
+});
